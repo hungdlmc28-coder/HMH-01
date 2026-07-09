efb499e2ecbab40f3552fe71b2c28bad8f927649
Ha Manh Hung <13686398+hungdlmc28-coder@users.noreply.github.com>
2026-07-09 11:36:06 +0700

    CF-002E: initialize SDK package

---
+++ src/qrp/sdk/__init__.py
@@ -0,0 +1,10 @@
+"""
+Software Development Kit (SDK).
+
+This package provides extension points and developer-facing
+interfaces for building on top of the Quant Research Platform.
+"""
+
+from __future__ import annotations
+
+__all__: tuple[str, ...] = ()
