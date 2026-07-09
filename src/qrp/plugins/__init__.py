53a5084e4f877fcdded4b9e3742c66fcb05df0b1
Ha Manh Hung <13686398+hungdlmc28-coder@users.noreply.github.com>
2026-07-09 11:39:27 +0700

    CF-002F: initialize plugin package

---
+++ src/qrp/plugins/__init__.py
@@ -0,0 +1,10 @@
+"""
+Plugin system.
+
+This package provides the extension namespace for
+plugins built on top of the Quant Research Platform.
+"""
+
+from __future__ import annotations
+
+__all__: tuple[str, ...] = ()
